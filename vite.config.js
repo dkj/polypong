5826e2cb1d9901755dc784da590f2a8d1662e829
David K. Jackson <david.jackson@sanger.ac.uk>
2026-01-04 19:37:18 +0000

    icon tweaks

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,13 @@ export default defineConfig({
                         src: 'pwa-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
-                        purpose: 'any maskable'
+                        purpose: 'maskable'
+                    },
+                    {
+                        src: 'favicon.svg',
+                        sizes: 'any',
+                        type: 'image/svg+xml',
+                        purpose: 'any'
                     }
                 ]
             },
